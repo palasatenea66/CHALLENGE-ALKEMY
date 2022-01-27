87de55b219e4860140b4f8cbe32da7b1f75f958c
palasatenea66 <romoanahi@gmail.com>
2022-01-27 04:27:26 -0300

    Estado inicial, código no terminado, faltan logs, try/excepts, completar contenido para tablas 2 y 3 y todo lo de deploy

---
+++ ARCHIVOS_SQL/crear_tabla1.sql
@@ -0,0 +1,15 @@
+CREATE TABLE tabla1 (
+Cod_Loc int,
+IdProvincia int,
+IdDepartamento int,
+Categoría varchar(50),
+Provincia varchar(20),
+Localidad varchar(20),
+Nombre varchar(50),
+Domicilio varchar(50),
+Cod_postal varchar(5),
+Num_teléfono varchar(15),
+Mail varchar(50),
+Web varchar (50),
+Fecha_carga date
+);
